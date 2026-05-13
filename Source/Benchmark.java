25c231ec5eaa283823ecbbb428a3227736ed340d
nods21 <noah.disch@gmail.com>
2026-05-13 10:45:47 +0200

    Modified Java Build version in pom.xml and added annotations for Benchmark.java (not working yet)

--- Source/Benchmark.java
+++ Source/Benchmark.java
@@ -1,4 +1,22 @@
+import org.openjdk.jmh.annotations.*;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 3, time = 1)
+@Measurement(iterations = 5, time = 1)
+@Fork(1)
 public class ArraySortBenchmark
 {
+    @Param({10, 100, 1000, 10000, 1000000})
+    private int size;
 
+    @Benchmark
+    public void benchmarkSequentialSort()
+    {
+
+    }
 }
